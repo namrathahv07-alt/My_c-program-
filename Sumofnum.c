f954fce401dbc64df742cc20a9c91ef19a066ab7
Namratha hv <namrathahv07@gmail.com>
2025-11-17 16:20:03 +0530

    Add C program to calculate the sum of two numbers

---
+++ Sumofnum.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a,b,sum;
+    printf("enter two numbers ");
+    scanf ("%d%d",&a,&b);
+    sum=a+b;
+    printf("sum=%d\n",sum);
+    return 0;
+}
