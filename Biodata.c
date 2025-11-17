bf5211175e7495fbbed8e9b010bb34ebac113159
Namratha hv <namrathahv07@gmail.com>
2025-11-17 19:42:17 +0530

    Create Biodata.c

---
+++ Biodata.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    printf("name: namratha hv\n");
+    printf ("age:18 years\n");
+    printf ("location: tumkur\n");
+
+    return 0;
+}
