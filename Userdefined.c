53f5033a37817ac32370021a126ff510fcabef0b
Namratha hv <namrathahv07@gmail.com>
2025-11-18 16:13:54 +0530

    Create Userdefined.c

---
+++ Userdefined.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int userdefined (){
+    char name,dept;
+    int age;
+    printf("my name is namratha\n");
+    printf("my age is 17\n");
+    printf("my dept is cse\n");
+}
+int main() {
+    userdefined();
+}
