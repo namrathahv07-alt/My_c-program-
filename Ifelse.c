8dcd3d8cd3876e60968eb174b3c2b5b171bddfc0
Namratha hv <namrathahv07@gmail.com>
2025-11-18 11:04:57 +0530

    Add eligibility check program in Ifelse.c

---
+++ Ifelse.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("enter a number");
+    scanf("%d",&num);
+    if (num>18){
+      printf("person is eligible");
+    }
+    else{
+        printf("person is not eligible ");
+    }
+    return 0;
+}
