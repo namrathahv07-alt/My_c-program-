d8abe801f71d56bcb360d3f39c985a19f55ddac2
Namratha hv <namrathahv07@gmail.com>
2025-11-18 11:53:51 +0530

    Create Break.c to illustrate break statement usage
    Add Break.c to demonstrate the use of the break statement in a loop.

---
+++ Break.c
@@ -0,0 +1,23 @@
+
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 5; i++) {
+        if (i == 3) {
+            break;   // loop stops when i becomes 3
+        }
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
+
+
+
+
+
+.
+
