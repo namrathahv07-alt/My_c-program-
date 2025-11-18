30a2344e0ba967e182c80878872fc0484f2d52ae
Namratha hv <namrathahv07@gmail.com>
2025-11-18 16:03:03 +0530

    Add Addfunctions.c for basic addition functionality

---
+++ Addfunctions.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int addition (int a,int b){
+    int c=a+b;
+    printf("%d\n",c);
+    return c;
+}
+int main() {
+    addition(2,3);
+}
