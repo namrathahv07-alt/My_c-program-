49f9d217008939ab0d7f087f7d544917a7aa2c59
Namratha hv <namrathahv07@gmail.com>
2025-11-18 11:31:42 +0530

    Add Nested.c for eligibility check program

---
+++ Nested.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("enter a number");
+    scanf("%d",&num);
+    if (num<18){
+       printf ("person is not eligible ");
+        if (num>18){
+            printf("person is eligible ");
+        }
+    }
+
+    return 0;
+}
