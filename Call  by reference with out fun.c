bc7bc0d97bea0e0c84036ddb841b2da74129f50e
Namratha hv <namrathahv07@gmail.com>
2025-11-20 13:08:10 +0530

    Create Call  by reference with out fun.c

---
+++ Call  by reference with out fun.c
@@ -0,0 +1,16 @@
+// call by reference without function 
+#include <stdio.h>
+
+int main() {
+    int a=10,b=20;
+    int *p1,*p2,temp;
+    p1=&a;
+    p2=&b;
+    printf(" %d %d\n",a,b);
+    temp=*p1;
+    *p1=*p2;
+    *p2=temp;
+    printf("%d %d\n",a,b);
+
+    return 0;
+}
