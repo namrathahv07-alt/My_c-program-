409263ac99e2767e52c8481defb590a844b43b12
Namratha hv <namrathahv07@gmail.com>
2025-11-20 11:12:49 +0530

    Create strcat.c

---
+++ strcat.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <string.h>
+int main() {
+   char yashu[10];
+   printf("enter your string\n");
+   scanf ("%s",&yashu);
+   printf("befor concat %s",yashu);
+   strcat(yashu,"legend");
+   printf("after conact %s",yashu);
+}
